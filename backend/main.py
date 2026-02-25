2282096c4d7128fa533c9bbefcc5e00b6c826e0a
alejandromoncadag <alex.squashplayer@gmail.com>
2026-02-24 22:26:42 -0500

    fix: cors syntax

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
     allow_origin_regex=r"^https://[a-z0-9]+\.opticaolm\.pages\.dev$",
-    allow_credentials=True
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
